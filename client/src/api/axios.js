e9de4d1ff15e1fc93a7f5154907a67a326a874f7
Suzana2806 <ayshathulsuzana545@gmail.com>
2026-07-29 15:10:38 +0530

    point frontend to deployed backend URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://job-application-tracker-l8ws.onrender.com/api',
 });
 
 // Attach the token automatically to every request, if it exists
